3ea78fd9bf58a2547444298cd64a8130bacb57c5
matheeshaYapa <ylmatheesha@gmail.com>
2020-08-16 23:37:15 +0530

    signup login fixes

--- src/app/authorization/components/forgot-password/forgot-password.component.ts
+++ src/app/authorization/components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  forgotPasswordForm: FormGroup;
+
+  isSubmitButtonClicked: boolean;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  get forgotPasswordFormControl() {
+    return this.forgotPasswordForm.controls;
+  }
+
+  initializeForm() {
+    this.forgotPasswordForm = new FormGroup({
+      email: new FormControl(null, {validators: [Validators.required, Validators.email]})
+    });
+  }
+
+  onSubmit() {
+    this.isSubmitButtonClicked = true;
+    if (this.forgotPasswordForm.invalid) {
+      return;
+    }
+    this.router.navigate(['/']);
   }
 
 }
--- src/app/authorization/components/sign-up/sign-up.component.ts
+++ src/app/authorization/components/sign-up/sign-up.component.ts
@@ -33,11 +33,24 @@ export class SignUpComponent implements OnInit {
     this.signUpForm = new FormGroup({
       fullName: new FormControl(null, {validators: [Validators.required]}),
       email: new FormControl(null, {validators: [Validators.required, Validators.email]}),
-      password: new FormControl(null, {validators: [Validators.minLength(6)]}),
-      confirmPassword: new FormControl(null, {validators: [Validators.required]}),
-      mobileNo: new FormControl(null, {validators: [Validators.pattern(/^((?!(0))[0-9]{9})$/m)]}),
+      password: new FormControl(null, {validators: [Validators.minLength(7)]}),
+      confirmPassword: new FormControl(null, {validators: [Validators.required, this.checkConfirmPassword.bind(this)]}),
+      mobileNo: new FormControl(null, {validators: [Validators.pattern(/^((0)[0-9]{9})$/m)]}),
       gender: new FormControl(null, {validators: [Validators.required]}),
     });
+  }
+
+  checkConfirmPassword(confirmPasswordFormControl: FormControl) {
+    let password = '';
+    if (this.signUpForm) {
+      if (this.signUpFormControl.password.value !== null) {
+        password = this.signUpFormControl.password.value;
+      }
+    }
+    if (confirmPasswordFormControl.value !== password && confirmPasswordFormControl.value !== null && confirmPasswordFormControl.value !== '') {
+      return {checkConfirmPassword: true}
+    }
+    return null;
   }
 
   onSubmit() {
